a491682dbfe77e2a7ea7b31587cf19944677c8b5
William <william.evrard@hei.yncrea.fr>
2018-07-03 21:49:44 +0200

    In progress

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,24 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 
+var apiKey = "AIzaSyDAU_exWXE3Pq99vWF3zO3xVx7rhoym_QA";
+
+void homePage(){
+  MapView.setApiKey(apiKey);
+  runApp(new MaterialApp(
+    home: new HomeScreen(),
+  ));
+}
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => new _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final textController = new TextEditingController();
+
+  MapView mapView = new MapView();
+  CameraPosition cameraPosition;
+  var staticMapProvider = new StaticMapProvider(apiKey);
+  Uri staticMapUri;
+
+  List<Marker> markers = <Marker>[
+    new Marker("1", "HEI", 50.633901, 3.045088,
+        color: Colors.blue),
+    new Marker("2", "Le Bô Bar", 50.631462, 3.049565,
+        color: Colors.red),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cameraPosition =
+        new CameraPosition(new Location(50.627104, 3.039928), 2.0);
+    staticMapUri = staticMapProvider.getStaticUri(
+        new Location(50.627104, 3.039928), 16,
+        height: 600, width: 900, mapType: StaticMapViewType.roadmap);
+    textController.addListener(printSomeValues);
+  }
+
+  printSomeValues(){
+    print("Controller value = ${textController.text}");
+  }
+
+  @override
+  void dispose(){
+    textController.removeListener(printSomeValues);
+    textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('HomeScreen'),
+        title: Center(
+          child: Text("L-Ticket maps"),
+        )
       ),
-      body: new Center(
-        child: new Text('Welcome to Home.!'),
-      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            height: 300.0,
+            child: new Stack(
+              children: <Widget>[
+                new Center(
+                  child: Container(
+                    child: new Text(
+                      "Map should show here",
+                      textAlign: TextAlign.center,
+                    ),
+                    padding: const EdgeInsets.all(20.0),
+                  ),
+                ),
+                new InkWell(
+                  child: new Center(
+                    child: new Image.network(staticMapUri.toString()),
+                  ),
+                ),
+              ]
+            ),
+          ),
+          new Container(
+            padding: const EdgeInsets.only(top: 15.0, left: 25.0, right: 25.0),
+            child: TextField(
+              onChanged: (text)=> print("Mon départ $text"),
+              controller: textController,
+              decoration: InputDecoration(
+                labelText: 'Départ'
+              ),
+            ),
+          ),
+          new Container(
+            padding: const EdgeInsets.only(top: 15.0, left: 25.0, right: 25.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Arrivée'
+                ),
+              ),
+          )
+        ]
+      )
     );
   }
 }
+
+
+
+
+
+
+
+
 
 Widget _bodyMapPageWidget(){
   return new Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'homeScreen.dart';
+import 'package:map_view/map_view.dart';
 
+var apiKey = "AIzaSyDAU_exWXE3Pq99vWF3zO3xVx7rhoym_QA";
 void main() {
+  MapView.setApiKey(apiKey);
   runApp(new MaterialApp(
     home: new SplashScreen(),
     routes: <String, WidgetBuilder>{
@@ -18,7 +21,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   startTime() async {
-    var _duration = new Duration(seconds: 3);
+    var _duration = new Duration(seconds: 5);
     return new Timer(_duration, navigationPage);
   }
   void navigationPage() {
